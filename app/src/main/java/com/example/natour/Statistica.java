76a3e644a483e1ac486c6806d01eae38344b8e18
Lorenzo3264 <78816362+Lorenzo3264@users.noreply.github.com>
2021-12-20 12:22:39 +0100

    aggiunti attributi nella classe Statistica e aggiunti file per interfaccia login

--- app/src/main/java/com/example/natour/Statistica.java
+++ app/src/main/java/com/example/natour/Statistica.java
@@ -1,5 +1,11 @@
 package com.example.natour;
 
 public class Statistica {
-
+    private int numUtenti;
+    private int numLogin;
+    private int numSegnalazioni;
+    private int numRicerche;
+    private int numRecensioni;
+    private int numItinerari;
+    private int numFoto;
 }

1c39ea76b17f5399d3b5d2c27977d3fee7897ee2
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2021-12-20 12:16:05 +0100

    Classe foto con attributi creati

---
+++ app/src/main/java/com/example/natour/Foto.java
@@ -0,0 +1,11 @@
+package com.example.natour;
+
+import android.graphics.Point;
+
+public class Foto
+{
+    private String nome;
+    private String pixel;
+    private Point posizione; // object (x,y)
+    
+}

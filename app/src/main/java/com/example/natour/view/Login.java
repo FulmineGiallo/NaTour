07d62013a9f308e94dfd06bc9d0b2dc844d1e631
Lorenzo3264 <78816362+Lorenzo3264@users.noreply.github.com>
2021-12-22 22:45:32 +0100

    File aggiornati, adesso può buildare

--- app/src/main/java/com/example/natour/view/Login.java
+++ app/src/main/java/com/example/natour/view/Login.java
@@ -1,14 +1,15 @@
 package com.example.natour.view;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import com.example.natour.R;
 
 public class Login extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }

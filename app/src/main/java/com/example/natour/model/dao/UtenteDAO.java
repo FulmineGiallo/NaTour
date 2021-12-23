2a54f7af5e638943f794cb327162266eeb7f7420
Lorenzo3264 <78816362+Lorenzo3264@users.noreply.github.com>
2021-12-23 16:29:24 +0100

    aggiornato metodo utenteExist

--- app/src/main/java/com/example/natour/model/dao/UtenteDAO.java
+++ app/src/main/java/com/example/natour/model/dao/UtenteDAO.java
@@ -3,16 +3,37 @@ package com.example.natour.model.dao;
 import com.example.natour.daointerface.UtenteDaoInterface;
 import com.example.natour.model.Utente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
 public class UtenteDAO implements UtenteDaoInterface
 {
+    private final PreparedStatement login;
+    public UtenteDAO(Connection connection) throws SQLException{
+
+        login = connection.prepareStatement("SELECT Count(*) FROM Utente WHERE email = ? AND password = ?");
+    }
 
     @Override
     public int utenteExist(String email, String password) throws SQLException
     {
-        return 0;
+
+        int accesso; //se ritorna zero l'utente non esiste altrimenti 1
+
+        try{
+            login.setString(1,email);
+            login.setString(2,password);
+            ResultSet rs = login.executeQuery();
+            while(rs.next()){
+                accesso = rs.getInt("count");
+            }
+        }catch (SQLException error){
+            //TODO: Aggiungere risposta all'errore
+        }
+        return accesso;
     }
 
     @Override

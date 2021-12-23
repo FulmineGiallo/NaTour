4f82589f2f5182bce16ee645fc5e08f57682b051
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2021-12-23 16:05:37 +0100

    Aggiunti metodi alle classe UtenteDao

--- app/src/main/java/com/example/natour/daointerface/UtenteDaoInterface.java
+++ app/src/main/java/com/example/natour/daointerface/UtenteDaoInterface.java
@@ -1,4 +1,17 @@
 package com.example.natour.daointerface;
 
-public interface UtenteDaoInterface {
+import com.example.natour.model.Utente;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+public interface UtenteDaoInterface
+{
+    /* Metodo per vedere se un utente esiste */
+    int utenteExist(String email, String password)throws SQLException;
+
+    /* Registrazione Utente */
+    Utente creaUtente(String nome, String cognome, String email, String passowrd, Date data)throws SQLException;
+
+
 }
--- app/src/main/java/com/example/natour/model/dao/UtenteDAO.java
+++ app/src/main/java/com/example/natour/model/dao/UtenteDAO.java
@@ -1,7 +1,24 @@
 package com.example.natour.model.dao;
 
-public class UtenteDAO
+import com.example.natour.daointerface.UtenteDaoInterface;
+import com.example.natour.model.Utente;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+public class UtenteDAO implements UtenteDaoInterface
 {
 
+    @Override
+    public int utenteExist(String email, String password) throws SQLException
+    {
+        return 0;
+    }
+
+    @Override
+    public Utente creaUtente(String nome, String cognome, String email, String passowrd, Date data) throws SQLException
+    {
+        return null;
+    }
 }
 

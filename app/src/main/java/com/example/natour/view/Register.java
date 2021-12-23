42c51ace7fb42afb576adcdc818226a17c2d5b08
Lorenzo3264 <78816362+Lorenzo3264@users.noreply.github.com>
2021-12-23 15:25:11 +0100

    migliorata interfaccia login, aggiunta activity registrazione

---
+++ app/src/main/java/com/example/natour/view/Register.java
@@ -0,0 +1,14 @@
+package com.example.natour.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.natour.R;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

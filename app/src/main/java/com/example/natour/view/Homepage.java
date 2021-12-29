39a2988951fd906cb484b400e134453f33a341de
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2021-12-29 15:44:58 +0100

    Aggiunte icone nell'interfaccia Login

--- app/src/main/java/com/example/natour/view/Homepage.java
+++ app/src/main/java/com/example/natour/view/Homepage.java
@@ -16,7 +16,8 @@ import com.example.natour.R;
 public class Homepage extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
--- app/src/main/java/com/example/natour/view/Login.java
+++ app/src/main/java/com/example/natour/view/Login.java
@@ -2,15 +2,33 @@ package com.example.natour.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.natour.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity
+{
+    Button entra;
+    Intent homepageIntent;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        entra = findViewById(R.id.btn_login);
+        homepageIntent = new Intent(this, Homepage.class);
+
+        entra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(homepageIntent);
+            }
+        });
     }
 }

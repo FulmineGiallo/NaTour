0f9e8237da947ab9b955e257284b3d0b673b8b1c
Giovanni2700 <gio.bentivoglio@studenti.unina.it>
2021-12-30 16:16:49 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/natour/visualizza_itinerario.java
+++ app/src/main/java/com/example/natour/visualizza_itinerario.java
@@ -9,6 +9,6 @@ public class visualizza_itinerario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_itinerario);
+        setContentView(R.layout.activity_visualizza_itinerario);
     }
 }

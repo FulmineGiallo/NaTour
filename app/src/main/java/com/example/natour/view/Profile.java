71158d9d34045cc1a447b202301396c69f0c9311
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2021-12-30 15:54:48 +0100

    Aggiornamento Profile

--- app/src/main/java/com/example/natour/view/Profile.java
+++ app/src/main/java/com/example/natour/view/Profile.java
@@ -2,15 +2,18 @@ package com.example.natour.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.natour.R;
 
-public class profile extends AppCompatActivity
+public class Profile extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        
     }
 }

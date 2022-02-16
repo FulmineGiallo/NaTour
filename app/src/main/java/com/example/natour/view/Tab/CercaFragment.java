b4c91727f139bf4ef2268013e36c14766ed4ee5b
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2022-02-16 16:31:34 +0100

    Aggiornato CercaFragment, aggiunta pagina con filtro funzionante

--- app/src/main/java/com/example/natour/view/Tab/CercaFragment.java
+++ app/src/main/java/com/example/natour/view/Tab/CercaFragment.java
@@ -1,23 +1,40 @@
 package com.example.natour.view.Tab;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.natour.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.slider.Slider;
 
 
 public class CercaFragment extends Fragment {
 
+    private FloatingActionButton filtro;
+    private NavigationView filtraView;
+    private EditText posizione;
+    private EditText durata;
+    private Slider difficolta;
+    private SwitchCompat disabili;
+    private ImageView bottoneNascoto;
+
+
+
     public CercaFragment()
     {
-        // Required empty public constructor
-    }
 
+    }
 
     public static CercaFragment newInstance(String param1, String param2)
     {
@@ -40,4 +57,67 @@ public class CercaFragment extends Fragment {
 
         return inflater.inflate(R.layout.fragment_cerca, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        filtro = requireView().findViewById(R.id.btn_filter);
+        filtraView = requireView().findViewById(R.id.viewFiltro);
+        bottoneNascoto = requireView().findViewById(R.id.btn_nascotoFiltro);
+
+        difficolta = requireView().findViewById(R.id.slider_difficoltà);
+        difficolta.addOnChangeListener((slider, value, fromUser) ->
+        {
+            switch ((int) value)
+            {
+                case 1:
+                    slider.setThumbTintList(ColorStateList.valueOf(getResources().getColor(R.color.facile)));
+                    slider.setHaloTintList(ColorStateList.valueOf(getResources().getColor(R.color.facile)));
+                    break;
+                case 2:
+                    slider.setThumbTintList(ColorStateList.valueOf(getResources().getColor(R.color.dilettante)));
+                    slider.setHaloTintList(ColorStateList.valueOf(getResources().getColor(R.color.dilettante)));
+                    break;
+                case 3:
+                    slider.setThumbTintList(ColorStateList.valueOf(getResources().getColor(R.color.intermedio)));
+                    slider.setHaloTintList(ColorStateList.valueOf(getResources().getColor(R.color.intermedio)));
+                    break;
+                case 4:
+                    slider.setThumbTintList(ColorStateList.valueOf(getResources().getColor(R.color.difficile)));
+                    slider.setHaloTintList(ColorStateList.valueOf(getResources().getColor(R.color.difficile)));
+                    break;
+                case 5:
+                    slider.setThumbTintList(ColorStateList.valueOf(getResources().getColor(R.color.esperto)));
+                    slider.setHaloTintList(ColorStateList.valueOf(getResources().getColor(R.color.esperto)));
+                    break;
+            }
+        });
+
+        filtro.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                filtraView.setVisibility(View.VISIBLE);
+                bottoneNascoto.setVisibility(View.VISIBLE);
+            }
+        });
+        bottoneNascoto.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                filtraView.setVisibility(View.INVISIBLE);
+                bottoneNascoto.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+
+
+
+
+
 }

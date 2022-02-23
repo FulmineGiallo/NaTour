c19574b79cad6d625902240218018e12d6875d4c
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2022-02-23 11:52:36 +0100

    Bug FIX intent type GPX

--- app/src/main/java/com/example/natour/controller/ControllerItinerario.java
+++ app/src/main/java/com/example/natour/controller/ControllerItinerario.java
@@ -397,7 +397,7 @@ public class ControllerItinerario
     public void getGPXFromDevice()
     {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("gpx/*");
+        intent.setType(".xml");
         Log.i("LANCIO", "GPX");
         if (intent.resolveActivity(inserimentoItinerarioActivity.getPackageManager()) != null)
             inserimentoItinerarioActivity.startActivityForResult(intent, 20);

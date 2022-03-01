99e2730390dcf434c29f0fcbd2535dac8bd2197f
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2022-03-01 16:45:14 +0100

    Visualizza itinerario iniziata

--- app/src/main/java/com/example/natour/controller/ControllerVisualizzaItinerario.java
+++ app/src/main/java/com/example/natour/controller/ControllerVisualizzaItinerario.java
@@ -1,5 +1,7 @@
 package com.example.natour.controller;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 import android.util.Log;
 
 import com.amplifyframework.rx.RxAmplify;
@@ -8,7 +10,10 @@ import com.amplifyframework.storage.result.StorageDownloadFileResult;
 import com.example.natour.model.Itinerario;
 import com.example.natour.view.VisualizzaItinerario.VisualizzaItinerarioActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ControllerVisualizzaItinerario
 {
@@ -24,16 +29,41 @@ public class ControllerVisualizzaItinerario
     public void getWaypointsFromItinerario()
     {
         String key = itinerario.getNomeFile();
-
+        StringBuilder text = new StringBuilder();
+        File file = new File(getApplicationContext().getFilesDir() + "/" + key +".txt");
+        if (!file.exists())
+        {
+            file.mkdirs();
+        }
         RxStorageBinding.RxProgressAwareSingleOperation<StorageDownloadFileResult> download =
                 RxAmplify.Storage.downloadFile(
                         key,
-                        new File(activity.getCacheDir() + "/" + key +".txt")
+                        file
                 );
         download
                 .observeResult()
                 .subscribe(
-                        result -> Log.i("MyAmplifyApp", "Successfully downloaded: " + result.getFile().getName()),
+                        result ->
+                        {
+                            Log.i("MyAmplifyApp", "Successfully downloaded: " + result.getFile().getAbsolutePath());
+                            try
+                            {
+                                BufferedReader br = new BufferedReader(new FileReader(file));
+                                String line;
+
+                                while ((line = br.readLine()) != null)
+                                {
+                                    text.append(line);
+                                    text.append('\n');
+                                }
+                                br.close();
+                            }
+                            catch (IOException e)
+                            {
+                                //You'll need to add proper error handling here
+                            }
+                            Log.i("COORDINATE", String.valueOf(text));
+                        },
                         error -> Log.e("MyAmplifyApp",  "Download Failure", error)
                 );
 

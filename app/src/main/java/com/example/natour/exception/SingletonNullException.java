4ddee2b3f3f6191edbc4d7267afe96300aa24d8e
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2022-03-23 10:00:47 +0100

    Pulizia Codice

--- app/src/main/java/com/example/natour/exception/SingletonNullException.java
+++
@@ -1,9 +0,0 @@
-package com.example.natour.exception;
-
-public class SingletonNullException extends Exception
-{
-    public SingletonNullException(String error)
-    {
-        super(error);
-    }
-}
